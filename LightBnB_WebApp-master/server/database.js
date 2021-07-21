2a2507d176bf83686de8676bbc635ff5a560d346
Shaun <s-purslow@hotmail.com>
2021-07-21 21:33:02 +0000

    update getAllProperties function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -100,14 +100,51 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool
-      .query(`SELECT * FROM properties LIMIT $1`, [limit])
-      .then((result) => {
-        return (result.rows);
-    })
-    .catch((err) => {
-      console.log (err.message);
-    });
+  const queryParams = [];
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+  // city
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+  //owner
+  if (options.owner_id) {
+    queryParams.push(`%${options.owner_id}%`);
+    queryString += ` AND owner_id = $${queryParams.length}`;
+  }
+
+  //min price per night
+  if (options.minimum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night}%`);
+    queryString += `AND cost_per_night > $${queryParams.length}`;
+  }
+  //max price per night
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${options.maximum_price_per_night}%`);
+    queryString += `AND cost_per_night < $${queryParams.length}`;
+  }
+
+  queryString += `GROUP BY properties.id`;
+
+  //min rating
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += `HAVING avg(property_reviews_rating) >= ${queryParams.length}`;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  console.log(queryString, queryParams);
+
+  return pool.query(queryString, queryParams).then((res) => res.rows);
 };
 exports.getAllProperties = getAllProperties;
 
